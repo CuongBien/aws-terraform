b8a02a92b889f4e0d8331247e570ba3ee2679edd
Cuong Bien <biencaocuongg@gmail.com>
2025-12-20 18:32:05 +0700

    change ip

--- infrastructure_aws/variables.tf
+++ infrastructure_aws/variables.tf
@@ -42,7 +42,7 @@ variable "key_pair_name" {
 variable "my_ip" {
   description = "Your home or office IP address for SSH access"
   type        = string
-  default     = "117.3.54.230/32" 
+  default     = "172.16.252.250/32" 
 }
 
 # ===== BLUE/GREEN DEPLOYMENT VARIABLES =====
